c52afdcbc054d2676c9057ff8810c9016ca06e9e
lekksha <33218023+lekksha@users.noreply.github.com>
2026-07-21 14:58:53 +0700

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.go
@@ -0,0 +1,36 @@
+type Solution struct{}
+
+func (s *Solution) Encode(strs []string) string {
+	const delimiter string = "#"
+	res := ""
+	for _, str := range strs {
+		res = res + fmt.Sprintf("%v%v%v", len(str), delimiter, str)
+	}
+	return res
+}
+
+func (s *Solution) Decode(encoded string) []string {
+	const delimiter string = "#"
+	res := []string{}
+
+	prefix := "" 
+	curStr := ""
+	counter := 0
+	c := ""
+	i := 0
+	for i < len(encoded) {
+		c = string(encoded[i])
+		if c == delimiter {
+			counter, _ = strconv.Atoi(prefix)
+			prefix = ""
+			curStr = encoded[i+1:i+counter+1]
+			res = append(res, curStr)
+			i = i + counter
+			counter = 0
+		} else {
+			prefix = prefix + c
+		}
+		i++
+	}
+	return res
+}
